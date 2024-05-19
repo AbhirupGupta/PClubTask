e8b602c94288f4a44d335099125afcb76e5f8aac
AbhirupGupta <166757302+AbhirupGupta@users.noreply.github.com>
2024-05-20 01:50:45 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char * argv[]) {
     setName("compares expected and actual integer outputs");
     registerTestlibCmd(argc, argv);
-    int ja = ans.readInt();
-    int pa = ouf.readInt();
-    if (ja != pa)
-        quitf(_wa, "expected %d, found %d", ja, pa);
-    quitf(_ok, "answer is %d", ja);
+    int x = ans.readInt();
+    int y = ouf.readInt();
+    if (x != y)
+        quitf(_wa, "expected %d, found %d", x, y);
+    quitf(_ok, "answer is %d", x);
 }
