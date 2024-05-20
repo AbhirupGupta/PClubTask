422a5654288d79acf6f48ce8dd3823b0c4a9dcec
AbhirupGupta <166757302+AbhirupGupta@users.noreply.github.com>
2024-05-21 01:06:22 +0530

    changed Solution.cpp acc to Q

--- Solution.cpp
+++ Solution.cpp
@@ -2,36 +2,48 @@
 using namespace std;
 int k;
 multiset <int> total;
-int grid[5][5];
+int grid[4][4];
 void read(){
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
             cin>>grid[i][j];
         }
     }
 }
 
 void solve(int i,int j, multiset <int> dmg){
-    if(i==4 && j==4){
+    if(i==3 && j==3){
         int sum=0,idx=0;
-        dmg.insert(grid[4][4]);
-        for(auto p=dmg.begin();p!=dmg.end();++p){sum+=*p;}
+        dmg.insert(grid[3][3]);
+
+        for(auto p=dmg.begin();p!=dmg.end();++p)
+        {
+            sum+=*p;
+        }
         for(auto p= --dmg.end();idx<k;idx++,--p)sum-=*p;
         total.insert(sum);
     }
-    else if(i>=5 || j >= 5){return;}
+    else if(i>=4 || j >=4 || i<0 || j<0 || grid[i][j]==0){return;}
     dmg.insert(grid[i][j]);
-    solve(i+1,j,dmg);
+    int temp= grid[i][j];
+    grid[i][j]=0;
+    solve(i+1,j,dmg); 
     solve(i,j+1,dmg);
+    solve(i-1,j,dmg);
+    solve(i,j-1,dmg);
+    grid[i][j]=temp;
 }
 
 int main(){
+    int t; cin>>t;
+    while(t--){
     cin>>k;
     read();
     multiset <int> dmg;
     solve(0,0,dmg);
     auto val=total.begin();
-    cout<<*val;
+    cout<<*val<<endl;
+    }
     return 0;
 }
 
