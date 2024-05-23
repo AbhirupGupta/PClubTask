6bd53232c5dd2b1feb3ee22f22c9a77d809f6480
AbhirupGupta <166757302+AbhirupGupta@users.noreply.github.com>
2024-05-23 13:18:46 +0530

    Update generator.cpp

--- generator.cpp
+++ generator.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
   registerGen(argc, argv, 1);
-  int t=rnd.next(1, 1000);
+  int t=rnd.next(1, 100);
   cout << t << endl;
   while(t--){
     cout << rnd.next(0, 3) << endl;
