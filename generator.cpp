00a64137c231103c63764165082376e36c8bbd54
AbhirupGupta <166757302+AbhirupGupta@users.noreply.github.com>
2024-05-23 16:35:13 +0530

    Update generator.cpp

--- generator.cpp
+++ generator.cpp
@@ -5,14 +5,25 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  registerGen(argc, argv, 1);
-  int t=rnd.next(1, 100);
-  cout << t << endl;
-  while(t--){
-    cout << rnd.next(0, 3) << endl;
-    for(int i=0;i<16;i++){
-        cout << rnd.next(-10, 10) << " ";
+    registerGen(argc, argv, 1);
+    int t; t=rnd.next(1,100);
+    cout << t << endl;
+    while(t--){
+        
+        cout << rnd.next(0, 3) << endl;
+        for(int i=0;i<16;i++){
+            int x = rnd.next(1,3);
+            if(x % 2 == 1){
+                cout << rnd.next(-10, 0) << endl;
+                
+            }
+            else
+            {
+                cout << rnd.next(-10, 10) << endl;
+                
+            }
+            
+        }
+        
     }
-    cout << endl;
-  }
 }
