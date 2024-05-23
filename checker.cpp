68e5739d8abc8677559ae6fbaf7fd56f8bc89559
AbhirupGupta <166757302+AbhirupGupta@users.noreply.github.com>
2024-05-23 13:18:20 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,11 +1,37 @@
 #include "testlib.h"
-
-int main(int argc, char * argv[]) {
-    setName("compares expected and actual integer outputs");
+#include <vector>
+ 
+using namespace std;
+ 
+int main(int argc, char *argv[]) {
+    setName("compare two signed int%d's", 8 * int(sizeof(int)));
     registerTestlibCmd(argc, argv);
-    int x = ans.readInt();
-    int y = ouf.readInt();
-    if (x != y)
-        quitf(_wa, "expected %d, found %d", x, y);
-    quitf(_ok, "answer is %d", x);
+ 
+    vector<int> expected, available;
+ 
+    int count = 0;
+    while (!ans.seekEof() && !ouf.seekEof() && count < 100) {
+        int ja = ans.readInt();
+        int pa = ouf.readInt(0,200,"Final hitpoints");
+        expected.push_back(ja);
+        available.push_back(pa);
+        count++;
+    }
+ 
+    for (int i = 0; i < expected.size(); ++i) {
+        if (i >= available.size()) {
+            quitf(_wa, "More integers expected in output file");
+        }
+        if (expected[i] != available[i]) {
+            quitf(_wa, "expected %d, found %d at position %d", expected[i], available[i], i + 1);
+        }
+    }
+ 
+    if (available.size() > expected.size()) {
+        quitf(_wa, "Extra integers found in output file");
+    }
+ 
+    quitf(_ok, "Answer is correct for %d integers", count);
+ 
+    return 0;
 }
